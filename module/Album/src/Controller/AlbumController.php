3678c5ba7b6d095d13b8091a2051ccae53fc255f
Eric Richer <eric.richer@vistoconsulting.com>
2023-02-16 14:14:12 -0500

    After Routing and Controllers

---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Album\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+
+class AlbumController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
